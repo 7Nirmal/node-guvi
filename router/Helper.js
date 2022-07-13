3bb02413fd8e1735ef38edcb479c73cfa13bee06
7Nirmal <ra.nirmalkumar96@gmail.com>
2022-07-13 20:17:05 +0530

    object id

--- router/Helper.js
+++ router/Helper.js
@@ -1,4 +1,5 @@
 import { client } from "../index.js";
+import { ObjectId } from "mongodb";
 
 export function delMovie(id) {
     return client.db("movies").collection("movies").deleteOne({ id: id });
@@ -7,7 +8,7 @@ export function insertmovies(data) {
     return client.db("movies").collection("movies").insertMany(data);
 }
 export function getMoviebyId(id) {
-    return client.db("movies").collection("movies").findOne({ id: id });
+    return client.db("movies").collection("movies").findOne({ _id: ObjectId(id) });
 }
 export async function getAllmovies(req) {
     return await client.db("movies").collection("movies").find(req.query).toArray();
